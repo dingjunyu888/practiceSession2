48e0a387ed423c830f7739b87ae8483672c952bc
Olga Karpenko <okarpenko@usfca.edu>
2024-04-18 23:02:01 -0700

    Added starter code for coding questions.

---
+++ src/main/java/BinarySearchTree.java
@@ -0,0 +1,135 @@
+class BinarySearchTree {
+
+    /** An inner class representing a node in a binary search tree */
+    private class BSTNode {
+        int data; // value stored at the node
+        BSTNode left; // left subtree
+        BSTNode right; // right subtree
+
+        BSTNode(int newdata) {
+            data = newdata;
+        }
+    }
+
+    private BSTNode root; // the root of the tree, an instance variable of class BinarySearchTree
+
+    BinarySearchTree() {
+        root = null; // initially, the tree is empty
+    }
+
+
+    /**
+     * Check if the two trees, given by root1 and root2, are equal
+     * Returns true if the trees have the same structure,
+     * and the values at the corresponding nodes are the same.
+     * @param root1 root of the first tree
+     * @param root2 root of the second tree
+     * @return
+     */
+    public static boolean checkIfIdentical(BSTNode root1, BSTNode root2) {
+        // FILL IN CODE
+
+        return false;
+    }
+
+    /**
+     * Computes the sum of the leaf values in the tree with the given root
+     * @param root root of the tree
+     * @return sum of leaf values
+     */
+    public int sumLeaves(BSTNode root) {
+        // FILL IN CODE
+
+        return 0;
+    }
+
+
+    /**
+     * Returns the value of the largest element in the binary search tree
+     * @param root root of the tree
+     * @return value of the largest element
+     */
+    public int findLargest(BSTNode root) {
+        BSTNode current = root;
+        // FILL IN CODE: must be iterative
+
+        return 0; // change
+    }
+
+    /**
+     * Returns the value of the second largest element in the binary search tree
+     * @param root root of the tree
+     * @return value of the largest element
+     */
+    public int findSecondLargest(BSTNode root) {
+        // FILL IN CODE:
+
+        return 0; // change
+    }
+
+    /**
+     * Insert a given element into the BST tree
+     * @param elem element to insert into the BST tree
+     */
+    public void insert(int elem) {
+        root = insert(root, elem);
+    }
+
+    /**
+     * Insert elem into the tree with the given root
+     * @param tree root of a tree
+     * @param elem element to insert
+     * @return the root of a tree that contains the new node
+     */
+    private BSTNode insert(BSTNode tree, int elem) {
+        if (tree == null) {
+            return new BSTNode(elem);
+        }
+        if (elem < tree.data) {
+            tree.left = insert(tree.left, elem);
+            return tree;
+        } else {
+            tree.right = insert(tree.right, elem);
+            return tree;
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree tree1 = new BinarySearchTree();
+        tree1.insert(4);
+        tree1.insert(12);
+        tree1.insert(10);
+        tree1.insert(25);
+        tree1.insert(3);
+        tree1.insert(1);
+        /* tree1:
+               4
+          3       12
+       1       10    25
+
+        */
+
+        BinarySearchTree tree2 = new BinarySearchTree();
+        tree2.insert(4);
+        tree2.insert(12);
+        tree2.insert(10);
+        tree2.insert(25);
+        tree2.insert(3);
+        tree2.insert(1);
+
+        BinarySearchTree tree3 = new BinarySearchTree();
+        tree3.insert(4);
+        tree3.insert(1);
+        tree3.insert(3);
+        tree3.insert(25);
+        tree3.insert(10);
+        tree3.insert(12);
+
+        System.out.println(checkIfIdentical(tree1.root, tree2.root)); //true
+        System.out.println(checkIfIdentical(tree1.root, tree3.root)); // false
+
+        System.out.println(tree1.sumLeaves(tree1.root)); //36
+        System.out.println(tree1.findLargest(tree1.root)); //25
+        System.out.println(tree1.findSecondLargest(tree1.root)); //12
+    }
+}
---
+++ src/main/java/PrefixTree.java
@@ -0,0 +1,125 @@
+
+/** PrefixTree class */
+public class PrefixTree  {
+
+    // --------- Private class Node ------------
+    // Represents a node in a  prefix tree
+    private class Node {
+        Node children[]; // array of children (26 children)
+        boolean isWord; // true if by concatenating "edges" on the path from the root to this node, we get a valid word
+
+        Node() {
+            isWord = false;
+            children = new Node[26]; // initialize the array of children
+        }
+    }
+
+    private Node root; // the root of the tree
+
+    /**
+     * Default constructor
+     */
+    public PrefixTree() {
+        root = new Node();
+    }
+
+    /**
+     * Adds a given word to the dictionary.
+     *
+     * @param word the word to add to the dictionary
+     */
+    public void add(String word) {
+        add(word.toLowerCase(), root);
+    }
+
+    /**
+     * Returns a number of valid words in the tree
+     * @return number of words
+     */
+    public int numValidWords() {
+        return numValidWords(root);
+    }
+
+
+    /**
+     * Find words that end at the internal node, not a leaf node.
+     * For instance, if our tree has "app" and "apple", this method should
+     * print "app". Since "app" ends at an internal node, not at the leaf.
+
+     */
+    public void findWordsThatEndAtInternalNodes() {
+        StringBuilder prefixBuilder = new StringBuilder();
+        findWordsThatEndAtInternalNodes(prefixBuilder, root);
+    }
+
+    /**
+     * A private add method that adds a given string to the tree
+     *
+     * @param s    the string to add
+     * @param node the root of a  subtree where we want to add a new string
+     */
+    private void add(String s, Node node) {
+        if (s.isEmpty()) {
+            node.isWord = true;
+            return;
+        }
+
+        int index = (int) s.charAt(0) - (int) 'a';
+        if (node.children[index] == null)
+            node.children[index] = new Node();
+        add(s.substring(1), node.children[index]);
+    }
+
+    /**
+     * Find words that end at the internal node, not a leaf node.
+     * For instance, if our tree has "app" and "apple", this method should
+     * print "app".
+     * @param prefix string builder that holds the prefix
+     * @param node root of the subtree
+     */
+    private void findWordsThatEndAtInternalNodes(StringBuilder prefix, Node node) {
+        // FILL IN CODE
+
+    }
+
+
+    /**
+     * Counts valid words in the tree
+     * @param node root of the subtree
+     * @return number of valid words
+     */
+    private int numValidWords(Node node) {
+        int count = 0;
+        // FILL IN CODE: count the number valid words in the subtree given by node
+
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        PrefixTree tree = new PrefixTree();
+        tree.add("apple");
+        tree.add("app");
+        tree.add("dog");
+        tree.add("doge");
+        tree.add("document");
+        tree.add("cat");
+        tree.add("caterpillar");
+        tree.add("belong");
+        tree.add("belonging");
+        tree.add("belongings");
+
+        System.out.println(tree.numValidWords()); // 10
+        StringBuilder sb = new StringBuilder();
+        tree.findWordsThatEndAtInternalNodes(sb, tree.root);
+        /*
+          app
+          belong
+          belonging
+          cat
+          dog
+         */
+    }
+
+}
+
